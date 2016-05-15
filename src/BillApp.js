ec9b22e842fc3abe48ee4f1787eb412cdf4212b6
Vasily Shelkov <vasilydshelkov@gmail.com>
2016-05-15 18:19:43 +0100

    Removed React-Router

--- src/BillApp.js
+++ src/BillApp.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const BillApp = () => (
-  <div className="container">
-    <h1 className="ui center aligned icon header" style={{ marginTop: '20px' }}>
-      <i className="circular calculator icon"></i>
-      <div className="ui huge header">Your Bill</div>
-    </h1>
-  </div>
-);
+class BillApp extends Component {
+  componentDidMount() {
+    console.log('mounts');
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1 className="ui center aligned icon header" style={{ marginTop: '20px' }}>
+          <i className="circular calculator icon"></i>
+          <div className="ui huge header">Your Bill</div>
+        </h1>
+      </div>
+    );
+  }
+}
 
 export default BillApp;
